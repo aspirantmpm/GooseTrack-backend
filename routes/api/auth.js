10b9ad988dd77e3db4c7c117cd68ae27df19dca2
Vadym <aspirantmpm@gmail.com>
2023-06-22 16:37:17 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -20,6 +20,6 @@ router.get('/current', authenticate, ctrl.getCurrent);
 
 router.post('/logout', authenticate, ctrl.logout);
 
-router.patch('/upload', ctrl.updateAvatar);
+router.patch('/upload', authenticate, ctrl.updateAvatar);
 
 module.exports = router;
