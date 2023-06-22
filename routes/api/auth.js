fa4eac3e917a960e03b3231b6542fc498941dca3
Vadym <aspirantmpm@gmail.com>
2023-06-22 21:18:16 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const upload = require('../../middlewares/index');
+// const upload = require('../../middlewares/index');
 const ctrl = require('../../controllers/auth');
 
 const { schemas } = require('../../models/user');
@@ -20,6 +20,6 @@ router.get('/current', authenticate, ctrl.getCurrent);
 
 router.post('/logout', authenticate, ctrl.logout);
 
-router.patch('/upload', upload.single('avatar'), authenticate, ctrl.updateAvatar);
+// router.patch('/upload', upload.single('avatar'), authenticate, ctrl.updateAvatar);
 
 module.exports = router;
