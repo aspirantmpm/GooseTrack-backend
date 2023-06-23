9d98f75b3a761964f039450b44333ecc7aba2c7c
Vadym <aspirantmpm@gmail.com>
2023-06-23 06:32:36 +0300

    reviews

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -4,15 +4,17 @@ const express = require('express');
 
 const router = express.Router();
 
-const ctrl = require('../../controllers/userProfile');
+const ctrl = require('../../controllers/reviews');
 
 const { validateBody, isValidId, authenticate } = require('../../middlewares');
 
-const { schemas } = require('../../models/userProfile');
+const { schemas } = require('../../models/reviews');
 
 router.get('/', authenticate, ctrl.getAll);
 
-router.get('/:id', authenticate, isValidId, ctrl.getById);
+router.get('/:id', authenticate, isValidId, ctrl.getAllOwn);
+
+// router.get('/:id', authenticate, isValidId, ctrl.getById);
 
 // router.post('/upload', ctrl.getById);
 
@@ -22,16 +24,24 @@ router.put(
   '/:id',
   authenticate,
   isValidId,
-  validateBody(schemas.userProfileAddSchema),
+  validateBody(schemas.reviewsAddSchema),
   ctrl.updateById
 );
+
+// router.patch(
+//   '/:id/favorite',
+//   authenticate,
+//   isValidId,
+//   validateBody(schemas.updateFavoriteShema),
+//   ctrl.updateFavorite
+// );
 
 router.patch(
   '/:id/favorite',
   authenticate,
   isValidId,
-  validateBody(schemas.updateFavoriteShema),
-  ctrl.updateFavorite
+  validateBody(schemas.reviewsAddSchema),
+  ctrl.updateById
 );
 
 module.exports = router;
