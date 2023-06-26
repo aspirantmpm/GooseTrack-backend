5bfa022434832e997c057753e193185b3c2c2e0c
Freebo12 <Tumechko12@gmail.com>
2023-06-26 18:57:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
 const authRouter = require("./routes/api/auth");
 const reviewsRouter = require("./routes/api/reviews");
@@ -10,7 +10,7 @@ const userRouter = require("./routes/api/user");
 
 const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
@@ -22,11 +22,11 @@ app.use("/api/task", taskRouter);
 app.use("/api/user", userRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
