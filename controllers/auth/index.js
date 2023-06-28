a976397c454abab5b5186ca8eb226e30d50272f0
Vadym <aspirantmpm@gmail.com>
2023-06-28 19:32:24 +0300

    fixx

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,7 +1,7 @@
 const { register } = require("./register");
 const { login } = require("./login");
 const { logout } = require("./logout");
-const { current } = require('./current');
+const { getCurrent } = require('./current');
 const { updateUser } = require("./update");
 const { refreshToken } = require("./refreshToken");
 const { verifyEmail } = require("./verifyEmail");
@@ -11,7 +11,7 @@ module.exports = {
   register,
   login,
   logout,
-  current,
+  getCurrent,
   updateUser,
   refreshToken,
   verifyEmail,
