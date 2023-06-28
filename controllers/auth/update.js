c3775141687593d167d0c9307be3041a22e65c4e
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-06-28 17:04:11 +0300

    added require cloudinary

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -1,6 +1,7 @@
 const { ctrlWrapper, HttpError } = require("../../helpers");
 const { User } = require("../../models/user");
 const { userDataValidator } = require("../../middlewares/userValidation");
+const cloudinary = require("cloudinary").v2;
 
 const updateUser = async (req, res, next) => {
   const { _id } = req.params;
