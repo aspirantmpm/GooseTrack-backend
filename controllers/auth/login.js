eb366d5e69ae518b995f6a82d8e18adb2fc0210a
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-06-28 22:50:07 +0300

    Update login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -25,10 +25,7 @@ const login = async (req, res) => {
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
-  await User.findByIdAndUpdate(
-    user._id,
-    { token }
-  );
+  await User.findByIdAndUpdate(user._id, { token });
 
   const userData = {
     id: user._id,
