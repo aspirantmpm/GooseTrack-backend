44b2b7d9feb585b4e433d2a5c765691ba65d5f9b
Vadym <aspirantmpm@gmail.com>
2023-06-28 12:44:19 +0300

    Update current.js

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,8 +1,8 @@
 const { ctrlWrapper } = require('../../helpers');
 
-const getCurrent = async (req, res) => {
+const current = async (req, res) => {
   const { email, name } = req.user;
   res.json({ email, name });
 };
 
-module.exports = ctrlWrapper(getCurrent);
+module.exports = ctrlWrapper(current);
