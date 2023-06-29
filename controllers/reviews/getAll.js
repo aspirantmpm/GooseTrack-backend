13efe96ba309e1737e49ddbb73605a24ed2498e3
Vadym <aspirantmpm@gmail.com>
2023-06-29 09:59:02 +0300

    add ava

--- controllers/reviews/getAll.js
+++ controllers/reviews/getAll.js
@@ -3,11 +3,24 @@ const {
   // HttpError,
   ctrlWrapper } = require('../../helpers');
 
+// const getAll = async (req, res) => {
+//   const { page = 1, limit = 10 } = req.query;
+//   const skip = (page - 1) * limit;
+//   const result = await Reviews.find().skip(skip).limit(limit);
+//   res.json(result);
+// };
+
 const getAll = async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Reviews.find().skip(skip).limit(limit);
-  res.json(result);
+
+  try {
+    const reviews = await Reviews.find().skip(skip).limit(limit).populate('owner', 'name avatarURL');
+
+    res.json(reviews);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 module.exports = ctrlWrapper(getAll);
