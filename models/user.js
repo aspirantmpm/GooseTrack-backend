08211d14c3c3418174f632f19c2852d5afaf2e4e
Voitenko Ilya <79865476+IlyaVoitenko@users.noreply.github.com>
2023-06-29 15:03:23 +0300

    changed phoneRegexp

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const { handleMongooseError } = require("../helpers");
 const emailRegexp =
   /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
 const birthdayRegexp = /^((0[1-9]|[12]\d|3[01])\/(0[1-9]|1[012])\/\d{4})$/;
-const phoneRegexp = /^38 \(\d{3}\) \d{3} \d{2} \d{2}$/;
+const phoneRegexp = /^\+?\d{1,3}\s?\d{2}\s?\d{3}\s?\d{2}\s?\d{2}$/;
 
 const userSchema = new Schema({
   name: {
@@ -82,7 +82,7 @@ const updateUserSchema = Joi.object().keys({
   name: Joi.string().required(),
   birthday: Joi.string().pattern(birthdayRegexp).required(),
   phone: Joi.string().pattern(phoneRegexp).required(),
-  skype: Joi.string().required(),
+  skype: Joi.string(),
 });
 
 const schemas = {
