5e94d301c8a9989efe7dde0b79abb71eae822811
Vadym <aspirantmpm@gmail.com>
2023-06-29 10:52:10 +0300

    Update addReview.js

--- controllers/reviews/addReview.js
+++ controllers/reviews/addReview.js
@@ -5,8 +5,18 @@ const {
 
 const addReview = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Reviews.create({ ...req.body, owner });
-  res.status(201).json(result);
+  const existingReview = await Reviews.findOne({ owner });
+
+  if (existingReview) {
+    return res.status(409).json({ message: 'Review already exists for this user' });
+  }
+
+  try {
+    const result = await Reviews.create({ ...req.body, owner });
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 module.exports = ctrlWrapper(addReview);
