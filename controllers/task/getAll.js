00994ef3fa79e4dc4a5129c26b156096eb51fd78
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-06-30 22:42:03 +0300

    added task owner filter for fetching all tasks

--- controllers/task/getAll.js
+++ controllers/task/getAll.js
@@ -1,9 +1,10 @@
-const { Task } = require('../../models/task');
+const { Task } = require("../../models/task");
 
 const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
   const { page = 1, limit = 50 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Task.find().skip(skip).limit(limit);
+  const result = await Task.find({ owner }).skip(skip).limit(limit);
   res.json(result);
 };
 
